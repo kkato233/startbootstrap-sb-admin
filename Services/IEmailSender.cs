f5a44ffccdcacd068dafe1c3399f061bb48583e4
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2017-12-03 23:55:55 +0900

    ASP.NET MVC Core 2 の プロジェクト追加

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace startbootstrap_sb_admin.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
