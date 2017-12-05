1dac0189387405562d389b1ed7acd1c2ebd81d5d
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2017-12-06 00:57:55 +0900

    ユーザメンテ画面の追加と 管理者に自動的に Setup する画面の追加

---
+++ Controllers/SetupController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using startbootstrap_sb_admin.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace startbootstrap_sb_admin.Controllers
+{
+    [Authorize]
+    public class SetupController : Controller
+    {
+        public SetupController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public async Task<IActionResult> Index()
+        {
+            if ((await _userManager.GetUsersInRoleAsync("Admin")).Count() > 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(String q = null)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            
+            if ((await _roleManager.FindByNameAsync("Admin")) == null)
+            {
+                var result2 = await _roleManager.CreateAsync(new IdentityRole("Admin"));
+
+                if (!result2.Succeeded)
+                {
+                    return new RedirectToActionResult("Index", "Home", null);
+                }
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, "Admin");
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["Error"] = "Error ";
+
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/UserMenteController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using startbootstrap_sb_admin.Models;
+using startbootstrap_sb_admin.Services;
+using Microsoft.Extensions.Logging;
+using startbootstrap_sb_admin.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace startbootstrap_sb_admin.Controllers
+{
+    /// <summary>
+    /// TODO:本当は権限チェックが必要
+    /// </summary>
+    [Authorize(Roles ="Admin")]
+    public class UserMenteController : Controller
+    {
+        public UserMenteController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ApplicationDbContext db,
+            ILogger<AccountController> logger)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger _logger;
+        private readonly ApplicationDbContext _db;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+
+            return View();
+        }
+    }
+}
